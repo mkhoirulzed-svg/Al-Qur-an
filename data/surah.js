e6026de50fc0a041d702eeba05103f460781ccee
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-23 07:25:44 +0700

    Create surah.js

---
+++ data/surah.js
@@ -0,0 +1,8 @@
+const surahList = [
+  { nomor: 1, nama: "Al-Fatihah", arti: "Pembukaan", ayat: 7 },
+  { nomor: 2, nama: "Al-Baqarah", arti: "Sapi Betina", ayat: 286 },
+  { nomor: 3, nama: "Ali 'Imran", arti: "Keluarga Imran", ayat: 200 },
+  { nomor: 4, nama: "An-Nisa'", arti: "Wanita", ayat: 176 },
+  { nomor: 5, nama: "Al-Ma'idah", arti: "Hidangan", ayat: 120 },
+  // nanti bisa ditambah sampai 114
+];
