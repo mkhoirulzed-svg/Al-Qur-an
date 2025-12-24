5d8be2e28ce09595d05f900d1a8178d59839a2f6
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-24 22:36:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,39 @@
-const CACHE_NAME = "islam-pwa-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const CACHE_NAME = "salam-app-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
