02721ac087d89c66474e904a36bc00e58a12f244
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-26 11:33:02 +0700

    Create kota.js

---
+++ data/kota.js
@@ -0,0 +1,75 @@
+// data/kota.js
+const KOTA_INDONESIA = [
+  // Jawa
+  "Jakarta",
+  "Bogor",
+  "Depok",
+  "Tangerang",
+  "Bekasi",
+  "Bandung",
+  "Cirebon",
+  "Semarang",
+  "Solo",
+  "Yogyakarta",
+  "Surabaya",
+  "Malang",
+  "Kediri",
+  "Madiun",
+  "Blitar",
+
+  // Sumatra
+  "Medan",
+  "Binjai",
+  "Padang",
+  "Bukittinggi",
+  "Pekanbaru",
+  "Dumai",
+  "Jambi",
+  "Palembang",
+  "Bengkulu",
+  "Bandar Lampung",
+  "Metro",
+  "Pangkal Pinang",
+
+  // Kalimantan
+  "Pontianak",
+  "Singkawang",
+  "Palangka Raya",
+  "Banjarmasin",
+  "Banjarbaru",
+  "Samarinda",
+  "Balikpapan",
+  "Bontang",
+  "Tarakan",
+
+  // Sulawesi
+  "Makassar",
+  "Parepare",
+  "Palopo",
+  "Manado",
+  "Bitung",
+  "Tomohon",
+  "Gorontalo",
+  "Palu",
+  "Kendari",
+  "Baubau",
+  "Mamuju",
+
+  // Bali & Nusa Tenggara
+  "Denpasar",
+  "Singaraja",
+  "Mataram",
+  "Bima",
+  "Kupang",
+
+  // Maluku
+  "Ambon",
+  "Tual",
+
+  // Papua
+  "Jayapura",
+  "Merauke",
+  "Timika",
+  "Sorong",
+  "Manokwari"
+];
