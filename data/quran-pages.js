26f1fec61636c01c0c010f4cb8a971962f418250
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2025-12-26 17:25:41 +0700

    Create quran-pages.js

---
+++ data/quran-pages.js
@@ -0,0 +1,115 @@
+// data/quran-pages.js
+const QURAN_PAGES = {
+  1: [
+    { surah: 1, ayat: 1 },
+    { surah: 1, ayat: 2 },
+    { surah: 1, ayat: 3 },
+    { surah: 1, ayat: 4 },
+    { surah: 1, ayat: 5 },
+    { surah: 1, ayat: 6 },
+    { surah: 1, ayat: 7 }
+  ],
+  2: [
+    { surah: 2, ayat: 1 },
+    { surah: 2, ayat: 2 },
+    { surah: 2, ayat: 3 },
+    { surah: 2, ayat: 4 },
+    { surah: 2, ayat: 5 }
+  ],
+  3: [
+    { surah: 2, ayat: 6 },
+    { surah: 2, ayat: 7 },
+    { surah: 2, ayat: 8 },
+    { surah: 2, ayat: 9 },
+    { surah: 2, ayat: 10 },
+    { surah: 2, ayat: 11 },
+    { surah: 2, ayat: 12 }
+  ],
+  4: [
+    { surah: 2, ayat: 13 },
+    { surah: 2, ayat: 14 },
+    { surah: 2, ayat: 15 },
+    { surah: 2, ayat: 16 },
+    { surah: 2, ayat: 17 },
+    { surah: 2, ayat: 18 },
+    { surah: 2, ayat: 19 },
+    { surah: 2, ayat: 20 }
+  ],
+  5: [
+    { surah: 2, ayat: 21 },
+    { surah: 2, ayat: 22 },
+    { surah: 2, ayat: 23 },
+    { surah: 2, ayat: 24 },
+    { surah: 2, ayat: 25 }
+  ],
+  6: [
+    { surah: 2, ayat: 26 },
+    { surah: 2, ayat: 27 },
+    { surah: 2, ayat: 28 },
+    { surah: 2, ayat: 29 }
+  ],
+  7: [
+    { surah: 2, ayat: 30 },
+    { surah: 2, ayat: 31 },
+    { surah: 2, ayat: 32 },
+    { surah: 2, ayat: 33 },
+    { surah: 2, ayat: 34 }
+  ],
+  8: [
+    { surah: 2, ayat: 35 },
+    { surah: 2, ayat: 36 },
+    { surah: 2, ayat: 37 },
+    { surah: 2, ayat: 38 }
+  ],
+  9: [
+    { surah: 2, ayat: 39 },
+    { surah: 2, ayat: 40 },
+    { surah: 2, ayat: 41 },
+    { surah: 2, ayat: 42 },
+    { surah: 2, ayat: 43 }
+  ],
+  10: [
+    { surah: 2, ayat: 44 },
+    { surah: 2, ayat: 45 },
+    { surah: 2, ayat: 46 }
+  ],
+  11: [
+    { surah: 2, ayat: 47 },
+    { surah: 2, ayat: 48 }
+  ],
+  12: [
+    { surah: 2, ayat: 49 },
+    { surah: 2, ayat: 50 }
+  ],
+  13: [
+    { surah: 2, ayat: 51 },
+    { surah: 2, ayat: 52 }
+  ],
+  14: [
+    { surah: 2, ayat: 53 },
+    { surah: 2, ayat: 54 }
+  ],
+  15: [
+    { surah: 2, ayat: 55 },
+    { surah: 2, ayat: 56 }
+  ],
+  16: [
+    { surah: 2, ayat: 57 },
+    { surah: 2, ayat: 58 }
+  ],
+  17: [
+    { surah: 2, ayat: 59 },
+    { surah: 2, ayat: 60 }
+  ],
+  18: [
+    { surah: 2, ayat: 61 },
+    { surah: 2, ayat: 62 }
+  ],
+  19: [
+    { surah: 2, ayat: 63 },
+    { surah: 2, ayat: 64 }
+  ],
+  20: [
+    { surah: 2, ayat: 65 },
+    { surah: 2, ayat: 66 }
+  ],
