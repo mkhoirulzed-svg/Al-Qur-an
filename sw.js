a60fefc2470fb938db85c8dbed8b36c1931aeee9
mkhoirulzed-svg <mkhoirulzed@gmail.com>
2026-06-29 09:14:36 +0700

    Refactor activate event listener in sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,6 @@ self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  self.clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
